4af86ff895ac68f29ce6b1fb759a8c2bbe6e259c
Hyun <hyun_7586@naver.com>
2024-12-02 06:56:19 +0900

    refactor(api): modify some changes
    - Service directory 구조 변경에 따른 import문 수정

--- src/main/java/org/example/todo/controller/token/TokenApiController.java
+++ src/main/java/org/example/todo/controller/token/TokenApiController.java
@@ -3,7 +3,7 @@ package org.example.todo.controller.token;
 import lombok.RequiredArgsConstructor;
 import org.example.todo.dto.token.CreateAccessTokenRequest;
 import org.example.todo.dto.token.CreateAccessTokenResponse;
-import org.example.todo.service.TokenService;
+import org.example.todo.service.token.TokenService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
